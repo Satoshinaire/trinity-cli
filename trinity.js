f1a7938f1c5f66fc3f980dc1ec866005eec36b8e
Satoshinaire <human@satoshinaire.com>
2017-11-07 23:53:01 +1100

    Add trade cancel command

--- trinity.js
+++ trinity.js
@@ -268,6 +268,14 @@ trinity
   })
 
 trinity
+  .command('trade cancel', 'Cancel an order on KuCoin.')
+  .help(commandHelp)
+  .action(function (args, cb) {
+    let self = this
+    trade.cancel(self, args, cb)
+  })
+
+trinity
   .command('version', 'Show Trinity version information.')
   .help(commandHelp)
   .action(function (args, cb) {
